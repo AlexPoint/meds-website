77c0f52eee2f5e0af465930f81eead6669202b3d
Alex <pointalexandre@gmail.com>
2016-06-22 23:29:50 +0200

    Basic client application with react

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import { Router, Route, hashHistory } from 'react-router';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={App}/>
+  </Router>
+, document.getElementById('app'))
